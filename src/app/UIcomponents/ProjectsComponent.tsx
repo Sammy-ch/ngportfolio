428aad5431379cfe269ce5d47c0efa491e5c2e11
Sammy-ch <sammystcherubin@gmail.com>
2024-08-12 11:40:04 +0200

    Updated Navbar

--- src/app/UIcomponents/ProjectsComponent.tsx
+++ src/app/UIcomponents/ProjectsComponent.tsx
@@ -4,7 +4,7 @@ function ProjectsComponent() {
   return (
     <main className='full-bleed p-10' id='portfolio'>
       <section className='full-bleed flex gap-5'>
-        <div className="h-96 w-full bg-[url('/portfolio/eden-port.png')] bg-contain bg-no-repeat rounded-lg"></div>
+        <div className="h-96  bg-[url('/portfolio/eden-port.png')] bg-contain bg-no-repeat rounded-lg"></div>
         <div className="h-96 w-full bg-[url('/portfolio/wasili-port.png')] bg-contain bg-no-repeat rounded-lg"></div>
         <div className="h-96 w-full bg-[url('/portfolio/rweru-port.png')] bg-contain bg-no-repeat rounded-lg"></div>
         <div className="h-96 w-full bg-[url('/portfolio/carhub-port.png')] bg-contain bg-no-repeat rounded-lg"></div>
